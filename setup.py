13abab51e105db2c537f892b6776b8f5cc6927b6
Fabian Affolter <fabian@affolter-engineering.ch>
2016-03-26 00:36:42 +0100

    Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Licensed under MIT. All rights reserved.
 from setuptools import setup
 
 setup(name='python-mystrom',
-      version='0.2.2',
+      version='0.2.3',
       description='Python API for controlling myStrom switches',
       url='https://github.com/fabaff/python-mystrom',
       author='Fabian Affolter',
