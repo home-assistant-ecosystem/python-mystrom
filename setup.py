5d32e61e869a8c5bdf3154dfcb6e342697f1d1e7
Fabian Affolter <fabian@affolter-engineering.ch>
2016-07-15 16:29:50 +0200

    Switch to find_packages()

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ Copyright (c) 2015-2016 Fabian Affolter <fabian@affolter-engineering.ch>
 
 Licensed under MIT. All rights reserved.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='python-mystrom',
-      version='0.3.5',
+      version='0.3.6',
       description='Python API for controlling myStrom switches',
       url='https://github.com/fabaff/python-mystrom',
       author='Fabian Affolter',
       author_email='fabian@affolter-engineering.ch',
       license='MIT',
       install_requires=['requests>=2.0'],
-      packages=['pymystrom'],
+      packages=find_packages(),
       zip_safe=True,
       include_package_data=True,
       )
