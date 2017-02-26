90fc90e192bb4e4d6a553c591b92c46023bbadf1
Fabian Affolter <mail@fabian-affolter.ch>
2017-02-26 11:53:40 +0100

    Fix output and update year

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2015-2016 Fabian Affolter <fabian@affolter-engineering.ch>
+Copyright (c) 2015-2017 Fabian Affolter <fabian@affolter-engineering.ch>
 
 Licensed under MIT. All rights reserved.
 """
@@ -13,15 +13,15 @@ STATE_ON = plug.get_relay_state()
 
 # Switch relay on if the plug is currently off
 if not STATE_ON:
-    print('Relay will be switched on.')
+    print("Relay will be switched on.")
     plug.set_relay_on()
     # Wait a few seconds to get a reading of the power consumption
-    print('Waiting for a couple of seconds...')
+    print("Waiting for a couple of seconds...")
     time.sleep(5)
 
 # Get the new state of the switch
-print('Relay state: ', plug.get_relay_state())
-print('Power consumption:', plug.get_consumption())
+print("Relay state:", plug.get_relay_state())
+print("Power consumption:", plug.get_consumption())
 
 # Switch relay off if it was off.
 if not STATE_ON:
