caebf6fcaa06998b316f218b2c69c85187bfa044
Fabian Affolter <fabian@affolter-engineering.ch>
2018-01-09 13:14:24 +0100

    Add new requirement

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 """
-Copyright (c) 2015-2017 Fabian Affolter <fabian@affolter-engineering.ch>
+Copyright (c) 2015-2018 Fabian Affolter <fabian@affolter-engineering.ch>
 
 Licensed under MIT. All rights reserved.
 """
 from setuptools import setup, find_packages
 
 setup(name='python-mystrom',
-      version='0.3.8',
+      version='0.4.0',
       description='Python API for interacting with myStrom devices',
       url='https://github.com/fabaff/python-mystrom',
       author='Fabian Affolter',
       author_email='fabian@affolter-engineering.ch',
       license='MIT',
-      install_requires=['requests>=2.0'],
+      install_requires=['requests>=2.0', 'click'],
       packages=find_packages(),
       zip_safe=True,
       include_package_data=True,
