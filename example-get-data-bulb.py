f554a1f51ba0cd80f5c86568f0e41b34eddf91e8
Fabian Affolter <fabian@affolter-engineering.ch>
2018-01-09 12:57:32 +0100

    Add more details

--- example-get-data-bulb.py
+++ example-get-data-bulb.py
@@ -8,7 +8,9 @@ import pymystrom
 bulb = pymystrom.MyStromBulb('192.168.0.51', '5CCF7FA0AFB0')
 
 # Get the details of the bulb
+print("Bulb state:", bulb.get_bulb_state())
 print("Current color:", bulb.get_color())
 print("Brightness:", bulb.get_brightness())
+print("Power consumption:", bulb.get_power())
 print("Transition time:", bulb.get_transition_time())
 print("Firmware version:", bulb.get_firmware())
