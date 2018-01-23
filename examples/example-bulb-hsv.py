275305e39c072bdc105c10799d5e3ea5345b2d1c
Fabian Affolter <fabian@affolter-engineering.ch>
2018-01-23 12:22:52 +0100

    Add a simple HSV example

---
+++ examples/example-bulb-hsv.py
@@ -0,0 +1,22 @@
+"""
+Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
+
+Licensed under MIT. All rights reserved.
+"""
+import time
+
+from pymystrom import bulb
+
+bulb = bulb.MyStromBulb('192.168.0.51', '5CCF7FA0AFB0')
+
+bulb.set_color_hex('000000FF')
+
+# Get the details of the bulb
+print("Current color details:", bulb.get_color())
+
+# Set color as HSV (Hue, Saturation, Value)
+bulb.set_color_hsv(50, 100, 100)
+time.sleep(3)
+
+# Shutdown the bulb
+bulb.set_off()
