db71d4cbd07f7226ef5c8b0096fca54d74bd71a0
Fabian Affolter <fabian@affolter-engineering.ch>
2018-01-23 11:12:21 +0100

    Update import

--- examples/example-switch.py
+++ examples/example-switch.py
@@ -4,9 +4,10 @@ Copyright (c) 2015-2018 Fabian Affolter <fabian@affolter-engineering.ch>
 Licensed under MIT. All rights reserved.
 """
 import time
-import pymystrom
 
-plug = pymystrom.MyStromPlug('10.100.0.137')
+from pymystrom import switch
+
+plug = switch.MyStromPlug('10.100.0.137')
 
 # Preserve state
 STATE_ON = plug.get_relay_state()
