3a84e8453c685ce6bc473432b8a2a2463d887c8e
Fabian Affolter <mail@fabian-affolter.ch>
2019-02-18 13:21:51 +0100

    Update docstring

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-"""
-Copyright (c) 2015-2018 Fabian Affolter <fabian@affolter-engineering.ch>
-
-Licensed under MIT. All rights reserved.
-"""
+"""Set up the Python API for myStrom devices."""
 import os
 
 import sys
