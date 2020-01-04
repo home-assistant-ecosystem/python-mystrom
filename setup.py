712cd4728243d36c15ee87b3f31b9f2bda8add1c
Fabian Affolter <mail@fabian-affolter.ch>
2020-01-04 21:28:43 +0100

    Use black for formatting

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme:
     long_description = readme.read()
 
-if sys.argv[-1] == 'publish':
-    os.system('python3 setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python3 setup.py sdist upload")
     sys.exit()
 
 setup(
-    name='python-mystrom',
-    version='0.5.0',
-    description='Python API for interacting with myStrom devices',
+    name="python-mystrom",
+    version="0.5.0",
+    description="Python API for interacting with myStrom devices",
     long_description=long_description,
-    url='https://github.com/fabaff/python-mystrom',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    license='MIT',
-    install_requires=['requests', 'click'],
+    url="https://github.com/fabaff/python-mystrom",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
+    install_requires=["requests", "click"],
     packages=find_packages(),
     zip_safe=True,
     include_package_data=True,
@@ -32,15 +32,15 @@ setup(
     mystrom=pymystrom.cli:main
 """,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
     ],
 )
