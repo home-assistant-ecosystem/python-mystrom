e359e2239649ad6b30609f58c724ba2330d033f7
Fabian Affolter <mail@fabian-affolter.ch>
2020-01-05 18:59:46 +0100

    Make the communication async

--- examples/example-bulb.py
+++ examples/example-bulb.py
@@ -1,48 +1,57 @@
 """Example code for communicating with a myStrom bulb."""
-import time
+import asyncio
 
-from pymystrom import bulb
+from pymystrom.bulb import MyStromBulb
 
-bulb = bulb.MyStromBulb('192.168.0.51', '5CCF7FA0AFB0')
+IP_ADDRESS = "192.168.0.50"
+MAC_ADDRESS = "5CCF7FA0C93B"
 
-# Preserve state
-STATE_ON = bulb.get_bulb_state()
 
-#  Switch bulb on if the bulb is currently off
-if not STATE_ON:
-    print("Bulb will be switched on with their previous setting")
-    bulb.set_on()
-    print("Waiting for a couple of seconds...")
-    time.sleep(2)
-    print("Bulb will be set to white")
-    bulb.set_white()
-    # Wait a few seconds to get a reading of the power consumption
-    print("Waiting for a couple of seconds...")
-    time.sleep(2)
-    # Set transition time to 2 s
-    bulb.set_transition_time(2000)
-    # Set to blue as HEX
-    bulb.set_color_hex('000000FF')
-    time.sleep(3)
-    # Set color as HSV (Hue, Saturation, Value)
-    bulb.set_color_hsv(50, 100, 100)
-    time.sleep(3)
+async def main():
+    """Sample code to work with a myStrom bulb."""
+    async with MyStromBulb(IP_ADDRESS, MAC_ADDRESS) as bulb:
+        print("Get the details from the bulb")
+        print(await bulb.get_bulb_state())
+        print("Bulb will be switched on with their previous setting")
+        await bulb.set_on()
+        # print("Waiting for a couple of seconds...")
+        await asyncio.sleep(2)
+        print("Bulb will be set to white")
+        await bulb.set_white()
+        # Wait a few seconds to get a reading of the power consumption
+        print("Waiting for a couple of seconds...")
 
-# Test a fast flashing sequence
-print("Flash it for 10 seconds")
-bulb.set_flashing(10, [100, 50, 30], [200, 0, 71])
-bulb.set_off()
+        await asyncio.sleep(2)
+        # Set transition time to 2 s
+        await bulb.set_transition_time(2000)
 
-# Test a fast flashing sequence
-print("Show a sunrise")
-bulb.set_sunrise(60)
+        # Set to blue as HEX
+        await bulb.set_color_hex("000000FF")
+        await asyncio.sleep(3)
 
-# Show a rainbow for 60 seconds
-print("Show a rainbow")
-bulb.set_rainbow(60)
+        # Set color as HSV (Hue, Saturation, Value)
+        await bulb.set_color_hsv(0, 0, 100)
+        await asyncio.sleep(3)
 
-# Reset transition time
-bulb.set_transition_time(1000)
+        # Test a fast flashing sequence
+        print("Flash it for 10 seconds...")
+        await bulb.set_flashing(10, [100, 50, 30], [200, 0, 71])
+        await bulb.set_off()
 
-# Shutdown the bulb
-bulb.set_off()
+        # Show a sunrise within a minute
+        print("Show a sunrise for 60 s")
+        await bulb.set_sunrise(60)
+
+        # Show a rainbow for 60 seconds
+        print("Show a rainbow")
+        await bulb.set_rainbow(60)
+        # Reset transition time
+        await bulb.set_transition_time(1000)
+
+        # Shutdown the bulb
+        await bulb.set_off()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- pymystrom/bulb.py
+++ pymystrom/bulb.py
@@ -1,116 +1,104 @@
 """Support for communicating with myStrom bulbs."""
-import time
+import asyncio
+import logging
 
-import requests
+import aiohttp
+from yarl import URL
 
-from . import exceptions
-from . import URI
+from . import _request as request
+
+_LOGGER = logging.getLogger(__name__)
+
+URI_BULB = URL("api/v1/device")
 
 
-class MyStromBulb(object):
+class MyStromBulb:
     """A class for a myStrom bulb."""
 
-    def __init__(self, host, mac):
+    def __init__(
+        self, host: str, mac: str, session: aiohttp.client.ClientSession = None,
+    ):
         """Initialize the bulb."""
-        self.resource = 'http://{}'.format(host)
+        self._close_session = False
+        self._host = host
         self._mac = mac
-        self.timeout = 5
-        self.data = None
-        self.state = None
-        self.consumption = 0
+        self._session = session
         self.brightness = 0
         self.color = None
+        self.consumption = 0
+        self.data = None
         self.firmware = None
         self.mode = None
+        self.state = None
         self.transition_time = 0
+        self.uri = URL.build(scheme="http", host=self._host).join(URI_BULB) / self._mac
 
-    def get_status(self):
-        """Get the details from the bulb."""
-        try:
-            request = requests.get(
-                '{}/{}/'.format(self.resource, URI), timeout=self.timeout)
-            raw_data = request.json()
-            # Doesn't always work !!!!!
-            #self._mac = next(iter(self.raw_data))
-            self.data = raw_data[self._mac]
-            return self.data
-        except (requests.exceptions.ConnectionError, ValueError):
-            raise exceptions.MyStromConnectionError()
+    async def get_bulb_state(self) -> object:
+        """Get the state of the bulb."""
+        response = await request(self, uri=self.uri)
+        print(response)
+        return bool(response["on"])
 
-    def get_bulb_state(self):
-        """Get the relay state."""
-        self.get_status()
-        try:
-            self.state = self.data['on']
-        except TypeError:
-            self.state = False
-
-        return bool(self.state)
-
-    def get_power(self):
-        """Get current power."""
-        self.get_status()
-        try:
-            self.consumption = self.data['power']
-        except TypeError:
-            self.consumption = 0
-
-        return self.consumption
-
-    def get_firmware(self):
-        """Get the current firmware version."""
-        self.get_status()
-        try:
-            self.firmware = self.data['fw_version']
-        except TypeError:
-            self.firmware = 'Unknown'
-
-        return self.firmware
-
-    def get_brightness(self):
-        """Get current brightness."""
-        self.get_status()
-        try:
-            self.brightness = self.data['color'].split(';')[-1]
-        except TypeError:
-            self.brightness = 0
-
-        return self.brightness
-
-    def get_transition_time(self):
-        """Get the transition time in ms."""
-        self.get_status()
-        try:
-            self.transition_time = self.data['ramp']
-        except TypeError:
-            self.transition_time = 0
-
-        return self.transition_time
-
-    def get_color(self):
-        """Get current color."""
-        self.get_status()
-        try:
-            self.color = self.data['color']
-            self.mode = self.data['mode']
-        except TypeError:
-            self.color = 0
-            self.mode = ''
-
-        return {'color': self.color, 'mode': self.mode}
-
-    def set_on(self):
+    # async def get_power(self):
+    #     """Get current power."""
+    #     await self.get_status()
+    #     try:
+    #         self.consumption = self.data['power']
+    #     except TypeError:
+    #         self.consumption = 0
+    #
+    #     return self.consumption
+    #
+    # async def get_firmware(self):
+    #     """Get the current firmware version."""
+    #     await self.get_status()
+    #     try:
+    #         self.firmware = self.data['fw_version']
+    #     except TypeError:
+    #         self.firmware = 'Unknown'
+    #
+    #     return self.firmware
+    #
+    # async def get_brightness(self):
+    #     """Get current brightness."""
+    #     await self.get_status()
+    #     try:
+    #         self.brightness = self.data['color'].split(';')[-1]
+    #     except TypeError:
+    #         self.brightness = 0
+    #
+    #     return self.brightness
+    #
+    # async def get_transition_time(self):
+    #     """Get the transition time in ms."""
+    #     await self.get_status()
+    #     try:
+    #         self.transition_time = self.data['ramp']
+    #     except TypeError:
+    #         self.transition_time = 0
+    #
+    #     return self.transition_time
+    #
+    # async def get_color(self):
+    #     """Get current color."""
+    #     await self.get_status()
+    #     try:
+    #         self.color = self.data['color']
+    #         self.mode = self.data['mode']
+    #     except TypeError:
+    #         self.color = 0
+    #         self.mode = ''
+    #
+    #     return {'color': self.color, 'mode': self.mode}
+    #
+    async def set_on(self):
         """Turn the bulb on with the previous settings."""
-        try:
-            request = requests.post(
-                '{}/{}/{}/'.format(self.resource, URI, self._mac),
-                data={'action': 'on'}, timeout=self.timeout)
-            if request.status_code == 200:
-                pass
-        except requests.exceptions.ConnectionError:
-            raise exceptions.MyStromConnectionError()
+        response = await request(
+            self, uri=self.uri, method="POST", data={"action": "on"}
+        )
+        return response
 
-    def set_color_hex(self, value):
+    async def set_color_hex(self, value):
         """Turn the bulb on with the given color as HEX.
 
         white: FF000000
@@ -119,83 +107,79 @@ class MyStromBulb(object):
         blue:  000000FF
         """
         data = {
-            'action': 'on',
-            'color': value,
+            "action": "on",
+            "color": value,
         }
+        response = await request(self, uri=self.uri, method="POST", data=data)
+        return response
 
-        try:
-            request = requests.post(
-                '{}/{}/{}/'.format(self.resource, URI, self._mac),
-                json=data, timeout=self.timeout)
-            if request.status_code == 200:
-                pass
-        except requests.exceptions.ConnectionError:
-            raise exceptions.MyStromConnectionError()
-
-    def set_color_hsv(self, hue, saturation, value):
+    async def set_color_hsv(self, hue, saturation, value):
         """Turn the bulb on with the given values as HSV."""
-        try:
-            data = "action=on&color={};{};{}".format(hue, saturation, value)
-            request = requests.post(
-                '{}/{}/{}'.format(self.resource, URI, self._mac),
-                data=data, timeout=self.timeout)
-            if request.status_code == 200:
-                self.data['on'] = True
-        except requests.exceptions.ConnectionError:
-            raise exceptions.MyStromConnectionError()
+        # The current situation doesn't allow to send JSON to the bulb as
+        # the firmware wants a string separated by ;. This is was
+        # reported in 2018 to myStrom
+        # data = {
+        #     'action': 'on',
+        #     'color': f"{hue};{saturation};{value}",
+        # }
+        data = "action=on&color={};{};{}".format(hue, saturation, value)
+        response = await request(self, uri=self.uri, method="POST", data=data)
+        return response
 
-    def set_white(self):
+    async def set_white(self):
         """Turn the bulb on, full white."""
-        self.set_color_hsv(0, 0, 100)
+        await self.set_color_hsv(0, 0, 100)
 
-    def set_rainbow(self, duration):
+    async def set_rainbow(self, duration):
         """Turn the bulb on and create a rainbow."""
         for i in range(0, 359):
-            self.set_color_hsv(i, 100, 100)
-            time.sleep(duration/359)
+            await self.set_color_hsv(i, 100, 100)
+            await asyncio.sleep(duration / 359)
 
-    def set_sunrise(self, duration):
-        """Turn the bulb on and create a sunrise."""
-        self.set_transition_time(duration/100)
+    async def set_sunrise(self, duration):
+        """Turn the bulb on and create a sunrise.
+
+        The brightness is from 0 till 100.
+        """
+        max_brightness = 100
+        await self.set_transition_time((duration / max_brightness))
         for i in range(0, duration):
-            try:
-                data = "action=on&color=3;{}".format(i)
-                request = requests.post(
-                    '{}/{}/{}'.format(self.resource, URI, self._mac),
-                    data=data, timeout=self.timeout)
-                if request.status_code == 200:
-                    self.data['on'] = True
-            except requests.exceptions.ConnectionError:
-                raise exceptions.MyStromConnectionError()
-            time.sleep(duration/100)
+            data = "action=on&color=3;{}".format(i)
+            await request(self, uri=self.uri, method="POST", data=data)
+            await asyncio.sleep(duration / max_brightness)
 
-    def set_flashing(self, duration, hsv1, hsv2):
+    async def set_flashing(self, duration, hsv1, hsv2):
         """Turn the bulb on, flashing with two colors."""
-        self.set_transition_time(100)
-        for step in range(0, int(duration/2)):
-            self.set_color_hsv(hsv1[0], hsv1[1], hsv1[2])
-            time.sleep(1)
-            self.set_color_hsv(hsv2[0], hsv2[1], hsv2[2])
-            time.sleep(1)
+        await self.set_transition_time(100)
+        for step in range(0, int(duration / 2)):
+            await self.set_color_hsv(hsv1[0], hsv1[1], hsv1[2])
+            await asyncio.sleep(1)
+            await self.set_color_hsv(hsv2[0], hsv2[1], hsv2[2])
+            await asyncio.sleep(1)
 
-    def set_transition_time(self, value):
+    async def set_transition_time(self, value):
         """Set the transition time in ms."""
-        try:
-            request = requests.post(
-                '{}/{}/{}/'.format(self.resource, URI, self._mac),
-                data={'ramp': value}, timeout=self.timeout)
-            if request.status_code == 200:
-                pass
-        except requests.exceptions.ConnectionError:
-            raise exceptions.MyStromConnectionError()
+        response = await request(
+            self, uri=self.uri, method="POST", data={"ramp": int(round(value))}
+        )
+        return response
 
-    def set_off(self):
+    async def set_off(self):
         """Turn the bulb off."""
-        try:
-            request = requests.post(
-                '{}/{}/{}/'.format(self.resource, URI, self._mac),
-                data={'action': 'off'}, timeout=self.timeout)
-            if request.status_code == 200:
-                pass
-        except requests.exceptions.ConnectionError:
-            raise exceptions.MyStromConnectionError()
+        response = await request(
+            self, uri=self.uri, method="POST", data={"action": "off"}
+        )
+        return response
+
+    async def close(self) -> None:
+        """Close an open client session."""
+        if self._session and self._close_session:
+            await self._session.close()
+
+    async def __aenter__(self) -> "MyStromBulb":
+        """Async enter."""
+        return self
+
+    async def __aexit__(self, *exc_info) -> None:
+        """Async exit."""
+        await self.close()
