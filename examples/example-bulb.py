ee9f24027635265be4f708abae21be6b22df402f
Fabian Affolter <mail@fabian-affolter.ch>
2020-11-09 17:44:14 +0100

    Add discovery support

--- examples/example-bulb.py
+++ examples/example-bulb.py
@@ -1,7 +1,9 @@
 """Example code for communicating with a myStrom bulb."""
 import asyncio
+import logging 
 
 from pymystrom.bulb import MyStromBulb
+from pymystrom.discovery import discover_devices
 
 IP_ADDRESS = "192.168.0.51"
 MAC_ADDRESS = "5CCF7FA0AFB0"
@@ -9,54 +11,24 @@ MAC_ADDRESS = "5CCF7FA0AFB0"
 
 async def main():
     """Sample code to work with a myStrom bulb."""
+    # Discover myStrom bulbs devices
+    devices = await discover_devices()
+
+    print(f"Found {len(devices)} bulb(s)")
+    for device in devices:
+        print(
+            f"  IP address: {device.host}, MAC address: {device.mac}"
+        )
+
     async with MyStromBulb(IP_ADDRESS, MAC_ADDRESS) as bulb:
         print("Get the details from the bulb...")
         await bulb.get_state()
 
         print("Power consumption:", bulb.consumption)
         print("Firmware:", bulb.firmware)
-        print("Current state:", bulb.state)
-
-        print("Bulb will be switched on with their previous setting")
-        await bulb.set_on()
-        # print("Waiting for a couple of seconds...")
-        await asyncio.sleep(2)
-        print("Bulb will be set to white")
-        await bulb.set_white()
-        # Wait a few seconds to get a reading of the power consumption
-        print("Waiting for a couple of seconds...")
-
-        await asyncio.sleep(2)
-        # Set transition time to 2 s
-        await bulb.set_transition_time(2000)
-
-        # Set to blue as HEX
-        await bulb.set_color_hex("000000FF")
-        await asyncio.sleep(3)
-
-        # Set color as HSV (Hue, Saturation, Value)
-        await bulb.set_color_hsv(0, 0, 100)
-        await asyncio.sleep(3)
-
-        # Test a fast flashing sequence
-        print("Flash it for 10 seconds...")
-        await bulb.set_flashing(10, [100, 50, 30], [200, 0, 71])
-        await bulb.set_off()
-
-        # Show a sunrise within a minute
-        print("Show a sunrise for 60 s")
-        await bulb.set_sunrise(60)
-
-        # Show a rainbow for 60 seconds
-        print("Show a rainbow")
-        await bulb.set_rainbow(60)
-        # Reset transition time
-        await bulb.set_transition_time(1000)
-
-        # Shutdown the bulb
-        await bulb.set_off()
-
+        print("Current state:", "off" if bulb.state is False else "on")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
---
+++ pymystrom/discovery.py
@@ -0,0 +1,113 @@
+"""Support for discovering myStrom devices."""
+import asyncio
+import logging
+from typing import Optional, List
+
+_LOGGER = logging.getLogger(__name__)
+
+DEVICE_MAPPING = {
+    "102": "myStrom Bulb",
+}
+
+class DiscoveredDevice(object):
+    """Representation of discovered device."""
+
+    mac: str
+    type: int
+    is_child: bool
+    mystrom_registered: bool
+    mystrom_online: bool
+    restarted: bool
+
+    @staticmethod
+    def create_from_announce_msg(raw_addr, announce_msg):
+        """Create announce message."""
+        _LOGGER.debug("Received announce message '%s' from %s ", announce_msg, raw_addr)
+        if len(announce_msg) != 8:
+            raise RuntimeError("Unexpected announcement, '%s'" % announce_msg)
+
+        device = DiscoveredDevice(host=raw_addr[0], mac=announce_msg[0:6].hex(":"))
+        device.type = announce_msg[6]
+
+        if device.type == '102':
+            device.hardware = DEVICE_MAPPING[str(announce_msg[6])]
+        else:
+            device.hardware = "non_mystrom"
+        status = announce_msg[7]
+
+        # Parse status field
+        device.is_child = status & 1 != 0
+        device.mystrom_registered = status & 2 != 0
+        device.mystrom_online = status & 4 != 0
+        device.restarted = status & 8 != 0
+        return device
+
+    def __init__(self, host, mac):
+        """Initialize the discovery."""
+        self.host = host
+        self.mac = mac
+
+
+class DeviceRegistry(object):
+    """Representation of the device registry."""
+
+    def __init__(self):
+        """Initialize the device registry."""
+        self.devices_by_mac = {}
+
+    def register(self, device):
+        """Register a device."""
+        self.devices_by_mac[device.mac] = device
+
+    def devices(self):
+        """Get all present devices"""
+        return list(self.devices_by_mac.values())
+
+
+class DiscoveryProtocol(asyncio.DatagramProtocol):
+    """Representation of the discovery protocol."""
+
+    def __init__(self, registry: DeviceRegistry):
+        """"Initialize the discovery protocol."""
+        super().__init__()
+        self.registry = registry
+
+    def connection_made(self, transport):
+        """Create an UDP listener."""
+        _LOGGER.debug("Starting up UDP listener")
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        """Handle a datagram."""
+        device = DiscoveredDevice.create_from_announce_msg(addr, data)
+        self.registry.register(device)
+
+    def connection_lost(self, exc: Optional[Exception]) -> None:
+        """Stop if connection is lost."""
+        _LOGGER.debug("Shutting down UDP listener")
+        super().connection_lost(exc)
+
+
+async def discover_devices(timeout: int = 7) -> List[DiscoveredDevice]:
+    """Discover local myStrom devices.
+    
+    Some myStrom devices report their presence every ~5 seconds in an UDP
+    broadcast to port 7979.
+    """
+    registry = DeviceRegistry()
+    loop = asyncio.get_event_loop()
+    (transport, protocol) = await loop.create_datagram_endpoint(
+        lambda: DiscoveryProtocol(registry), local_addr=("0.0.0.0", 7979)
+    )
+    # Server runs in the background, meanwhile wait until timeout expires
+    await asyncio.sleep(timeout)
+    # Shutdown server
+    transport.close()
+
+    devices = registry.devices()
+    for device in devices:
+        _LOGGER.debug(
+            "Discovered myStrom device %s (%s) (MAC addresse: %s)", device.host, device.type, device.mac
+        )
+    return devices
+
